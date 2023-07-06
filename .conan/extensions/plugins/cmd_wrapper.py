f1117052c17e4d77857dde142982e080c2a123b0
prince-chrismc <prince.chrismc@gmail.com>
2023-07-06 13:36:36 -0700

    rough draft for meassuring the time for a command

--- .conan/extensions/plugins/cmd_wrapper.py
+++ .conan/extensions/plugins/cmd_wrapper.py
@@ -1,4 +1,17 @@
+from conan.api.output import ConanOutput
+
 def cmd_wrapper(cmd, conanfile):
-    if "cmake" in cmd:
-        print(f'echo "{cmd}"')
+    if cmd.startswith("cmake --build"):
+        # https://github.com/conan-io/conan/blob/ae001d3da17fa9a76241fd09508d8040d55341d2/conan/tools/cmake/presets.py#L63
+        ConanOutput(str(conanfile)).info(f'BuildConsole /command="{cmd}"')
+        # new_cmd = cmd[6:].replace("\"","")
+        # print(new_cmd)
+        new_cmd = "', '".join(cmd.split(" "))
+        print(str(conanfile), new_cmd)
+        new_cmd = "PowerShell -NoProfile -ExecutionPolicy Bypass -Command Measure-Command {Start-Process cmake -ArgumentList  @('" + new_cmd + "') -wait | Out-Default}"
+        print(new_cmd)
+        return new_cmd
     return cmd
+
+
+# Possible specific targets, OpenCV, FFmpeg
