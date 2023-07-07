0fc1f2894ca463f3de73272cf66de0dafbd07f93
prince-chrismc <prince.chrismc@gmail.com>
2023-07-07 10:11:54 -0700

    switching to time and using extra jobs

--- .conan/extensions/plugins/cmd_wrapper.py
+++ .conan/extensions/plugins/cmd_wrapper.py
@@ -4,12 +4,10 @@ def cmd_wrapper(cmd, conanfile):
     if cmd.startswith("cmake --build"):
         # https://github.com/conan-io/conan/blob/ae001d3da17fa9a76241fd09508d8040d55341d2/conan/tools/cmake/presets.py#L63
         ConanOutput(str(conanfile)).info(f'BuildConsole /command="{cmd}"')
-        # new_cmd = cmd[6:].replace("\"","")
-        # print(new_cmd)
-        new_cmd = "', '".join(cmd.split(" "))
-        print(str(conanfile), new_cmd)
-        new_cmd = "PowerShell -NoProfile -ExecutionPolicy Bypass -Command Measure-Command {Start-Process cmake -ArgumentList  @('" + new_cmd + "') -wait | Out-Default}"
-        print(new_cmd)
+
+        # https://stackoverflow.com/a/58322616
+        new_cmd = cmd.replace('"','\\"')
+        new_cmd = "echo %TIME% & " + f'BuildConsole /command="{new_cmd} -j 18"' + " & cmd /v:on /c echo !TIME!"
         return new_cmd
     return cmd
 
